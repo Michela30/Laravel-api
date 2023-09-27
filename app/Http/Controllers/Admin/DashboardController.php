3536d86dbba44a1c1888ab8f08df291eb18e8421
Michela30 <micheladestefano30@gmail.com>
2023-09-27 18:10:47 +0200

    reactive dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//models
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
+
+
 // Helpers
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
+        $user = User::all();
+        $countProjects = Project::count();
+        $countTypes = Type::count();
+        $countTechnologies = Technology::count();
 
-        $countProjects = \App\Models\Project::count();
 
-        return view('dashboard');
+        return view('dashboard', compact('user','countProjects', 'countTypes', 'countTechnologies'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,14 +3,63 @@
 @section('page-title', 'Dashboard')
 
 @section('main-content')
+
+    <div class="text-center mb-5">
+        <h2 class="text-success">
+            Benvenuto!
+        </h2>
+    </div>
     <div class="row">
-        <div class="col">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="card">
                 <div class="card-body">
-                    <h1 class="text-center text-success">
-                        Sei loggato!
-                    </h1>
-                    <br>
+                    <h2 class="text-center text-success fs-4">
+                        Projects
+                    </h2>
+                    <h3 class="text-center fs-2">
+                        {{ $countProjects }}
+                    </h3>
+                    <div class="text-center mt-4">
+                        <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">
+                            View All
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="text-center text-success fs-4">
+                        Types
+                    </h2>
+                    <h3 class="text-center fs-2">
+                        {{ $countTypes }}
+                    </h3>
+                    <div class="text-center mt-4">
+                        <a href="{{ route('admin.types.index') }}" class="btn btn-primary">
+                            View All
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="text-center text-success fs-4">
+                        Technologies
+                    </h2>
+                    <h3 class="text-center fs-2">
+                        {{ $countTechnologies }}
+                    </h3>
+                    <div class="text-center mt-4">
+                        <a href="{{ route('admin.technologies.index') }}" class="btn btn-primary">
+                            View All
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
