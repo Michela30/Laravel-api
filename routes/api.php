307a8ec766ed083af74b13f52269e1143b1083a4
Michela30 <micheladestefano30@gmail.com>
2023-09-27 15:10:27 +0200

    start project

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::name('api.')->group(function (){
+
+    Route::resource('projects', ProjectController::class)->only(['index', 'show']);
+    
 });
